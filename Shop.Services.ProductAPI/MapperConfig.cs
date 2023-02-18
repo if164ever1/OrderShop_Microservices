30782435b3206c2f697b8a857accd3133350a675
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-18 20:38:44 +0100

    create automaperConfig and add automapper configuration to services

---
+++ Shop.Services.ProductAPI/MapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Shop.Services.ProductAPI.Models;
+using Shop.Services.ProductAPI.Models.Dto;
+
+namespace Shop.Services.ProductAPI
+{
+	public class MapperConfig
+	{
+		public static MapperConfiguration RegisterMappers() 
+		{
+			var mappingConfig = new MapperConfiguration(config => 
+			{
+				config.CreateMap<ProductDto, Product>().ReverseMap();
+			});
+
+			return mappingConfig;
+		}
+	}
+}
--- Shop.Services.ProductAPI/Program.cs
+++ Shop.Services.ProductAPI/Program.cs
@@ -1,12 +1,18 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Shop.Services.ProductAPI;
 using Shop.Services.ProductAPI.DbContaxts;
 using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//Connection to database
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+
+//configuration for automapping
+IMapper mapper = MapperConfig.RegisterMappers().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
 builder.Services.AddControllers();
