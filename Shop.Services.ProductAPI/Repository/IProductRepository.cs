6101616a5acb0ede7b52cdfb8e9db30432f83053
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-18 20:22:09 +0100

    create IProductRepository

---
+++ Shop.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Services.ProductAPI.Models.Dto;
+
+namespace Shop.Services.ProductAPI.Repository
+{
+	public interface IProductRepository
+	{
+		Task<IEnumerable<ProductDto>> GetAllAsync();
+		Task<ProductDto> GetProductByIdAsync(int productId);
+		Task<ProductDto> CreateUpdateProductAsync(ProductDto productDto);
+		Task<ProductDto> DeleteProductAsync(int productId);
+	}
+}
