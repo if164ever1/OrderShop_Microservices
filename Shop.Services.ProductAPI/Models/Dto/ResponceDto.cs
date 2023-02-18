8c184f8871dd79029138394fa190a8e397cd59c2
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-18 20:14:18 +0100

    create productdto

---
+++ Shop.Services.ProductAPI/Models/Dto/ResponceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Services.ProductAPI.Models.Dto
+{
+	public class ResponceDto
+	{
+		public bool IsSuccess { get; set; }
+		public object Result { get; set; }
+		public string DisplayMessage { get; set; }
+		public List<string> ErrorMesage { get; set; }
+	}
+}
