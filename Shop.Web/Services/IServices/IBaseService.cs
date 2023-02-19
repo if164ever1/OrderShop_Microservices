159e3c2ea12ada64c116c0c0ecbc6c6347f41913
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-19 18:04:33 +0100

    create Interface IBaseService

---
+++ Shop.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Shop.Web.Models;
+
+namespace Shop.Web.Services.IServices
+{
+	public interface IBaseService:IDisposable
+	{
+		ResponceDto responceModel { get; set; }
+		Task<T> SendAsync<T>(ApiRequest apiRequest);
+	}
+}
