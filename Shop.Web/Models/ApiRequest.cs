b2ebf8f467fdc525b78b1ce6c7b3d878e9005c74
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-19 18:04:17 +0100

    add model ApiRequest

---
+++ Shop.Web/Models/ApiRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Reflection.Metadata.Ecma335;
+using static Shop.Web.SD;
+
+namespace Shop.Web.Models
+{
+	public class ApiRequest
+	{
+		public ApiType ApiType { get; set; } = ApiType.GET;
+		public string Url { get; set; }
+		public object Data { get; set; }
+		public string AccessToken { get; set; }
+	}
+}
