56d8c8e04e642c4b93756f4762e8211fd5d6e4ab
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-19 13:41:34 +0100

    add the same dtos as in api project

---
+++ Shop.Web/Models/ResponceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Web.Models
+{
+	public class ResponceDto
+	{
+		public bool IsSuccess { get; set; } = true;
+		public object Result { get; set; }
+		public string DisplayMessage { get; set; }
+		public List<string> ErrorMesage { get; set; }
+	}
+}
