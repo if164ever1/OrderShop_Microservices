74957f35b4ad2454893ab2c509228b28e9c14df4
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2023-02-28 20:11:42 +0100

    setup di

--- Shop.Web/Program.cs
+++ Shop.Web/Program.cs
@@ -1,9 +1,19 @@
+using Shop.Web;
+using Shop.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DI
+builder.Services.AddHttpClient<IProductService, IProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+builder.Services.AddScoped<IProductService, IProductService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
